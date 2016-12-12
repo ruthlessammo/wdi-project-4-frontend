96ef56ac34e0d93bdeca0d9efda7348b35ae326f
William Hilton <williamjchilton@gmail.com>
2016-12-12 20:40:57 +0000

    begun styling

--- src/js/directives/soundcloud.js
+++ src/js/directives/soundcloud.js
@@ -10,7 +10,7 @@ function soundcloud() {
       width: '@',
       height: '@'
     },
-    template: '<iframe src="{{ src }}" width="{{ width }}" height="{{ height }}" scrolling="no" frameborder="no"></iframe>',
+    template: '<iframe src="{{ src }}" width="{{ width }}" height="{{ height }}" scrolling="no" frameborder="no" color=black_white></iframe>',
     link($scope) {
       $scope.src = `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${$scope.id}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true`;
     }
