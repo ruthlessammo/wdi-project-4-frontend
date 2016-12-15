c7b71679c4bbe3438726b79c5a16a34ff4383271
William Hilton <williamjchilton@gmail.com>
2016-12-15 15:44:44 +0000

    more confi for heroku

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 angular.module('finalProject', ['ngResource', 'ui.router', 'satellizer'])
-.constant('API_URL', window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : '//ruthlessmuzic.herokuapp.com')
+.constant('API_URL', window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : '//ruthlessmuzic.herokuapp.com/api')
   .config(Auth)
   .config(WhitelistSrc);
 
