4457f17b88d9b01698f4fee253384766c50aef6e
William Hilton <williamjchilton@gmail.com>
2016-12-19 12:15:55 +0000

    updated api url

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 angular.module('finalProject', ['ngResource', 'ui.router', 'satellizer'])
-.constant('API_URL', window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : '//ruthlessmuzic.herokuapp.com/api')
+.constant('API_URL', window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : '//ruthlessmuzic-api.herokuapp.com/api')
   .config(Auth)
   .config(WhitelistSrc);
 
